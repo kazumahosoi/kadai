a95cc773677570a1aaffd8650575f3e3889f82c9
k-hosoi <khosoi@ecc.ac.jp>
2023-02-20 15:33:18 +0900

    課題2、３、４のファイル追加

---
+++ kadai4/js/teacher.js
@@ -0,0 +1,7 @@
+$('#wave').wavify({
+  height: 100,      // 波の上限
+  bones: 5,         // 波の数
+  amplitude: 200,   // 波の振幅
+  color: 'red',     // 波の色
+  speed: .25        // 波のスピード (0.25)ぐらいがちょうどいい
+});
