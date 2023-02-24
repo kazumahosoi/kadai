62224d79cb7bb089d4b7dbf7a5d6f3695ea2d038
2220280 <2220280.ecc.ac.jp>
2023-02-24 10:46:45 +0900

    色変えたよ

--- kadai4/js/teamE.js
+++ kadai4/js/teamE.js
@@ -2,6 +2,6 @@ $('#wave').wavify({
   height: 110,      // 波の上限
   bones: 8,         // 波の数
   amplitude: 190,   // 波の振幅
-  color: 'blue',     // 波の色
+  color: 'white',     // 波の色
   speed: .25        // 波のスピード (0.25)ぐらいがちょうどいい
 });
